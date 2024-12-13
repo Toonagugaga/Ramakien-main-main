f149ba264175393dc209c3e19cf7d94cfc9836cb
toon <160430764+Toonagugaga@users.noreply.github.com>
2024-12-13 13:21:00 +0700

    first commit

---
+++ sinup.html/eye.js
@@ -0,0 +1,16 @@
+const pwShowHide = document.querySelectorAll(".eye-icon")
+// Add click event listener to each eye icon for toggling password visibility
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password")
+        pwFields.forEach(password => {
+            if (password.type === "password") { // If password is hidden
+                password.type = "text" // Show password
+                eyeIcon.classList.replace("bx-hide", "bx-show") // Change icon to show state
+                return
+            }
+            password.type = "password" // Hide password
+            eyeIcon.classList.replace("bx-show", "bx-hide") // Change icon to hide state
+        })
+    })
+})
